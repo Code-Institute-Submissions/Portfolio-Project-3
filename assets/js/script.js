181738fdbb5ba9cf7caffca65bf6aece5f279578
Francisco Sabino <fdasabino@gmail.com>
2021-05-16 16:39:10 +0200

    modal github profile incorporated

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,23 @@ function toggleMenu() {
 	menuToggle.classList.toggle("active");
 	menu.classList.toggle("active");
 }
+
+// modal-box
+
+let openbtn = document.querySelector(".js-open");
+let modalBg = document.getElementById("modal-bg");
+let modalBox = document.getElementById("modal-box");
+
+openbtn.addEventListener("click", function (event) {
+	event.preventDefault();
+	modalBg.classList.add("active");
+	modalBox.classList.add("active");
+});
+
+let closeBtns = document.querySelectorAll(".js-btn");
+closeBtns.forEach((node) => {
+	node.addEventListener("click", function (e) {
+		modalBg.classList.remove("active");
+		modalBox.classList.remove("active");
+	});
+});
