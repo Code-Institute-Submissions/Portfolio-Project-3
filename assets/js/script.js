22e13fb74a050284084d1ee98ea6e6c948f47d20
Francisco Sabino <fdasabino@gmail.com>
2021-05-29 02:59:58 +0200

    Page adjustments as well as form more interative

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,36 +31,45 @@ closeBtns.forEach((node) => {
 });
 
 // Send email form
+function validate() {
+	let name = document.getElementById("name").value;
+	let email = document.getElementById("email").value;
+	let message = document.getElementById("message").value;
+	let error_message = document.getElementById("error_message");
+	let sendButton = document.getElementById("myform");
 
-function EnableDisable(msg) {
-	//Reference the Button.
-	var btnSubmit = document.querySelector('input[type="submit"]');
+	sendButton.addEventListener("submit", function (event) {
+		event.preventDefault(sendButton);
+		var tempParams = {
+			from_name: document.getElementById("name").value,
+			from_email: document.getElementById("email").value,
+			message: document.getElementById("message").value,
+		};
+		emailjs
+			.send("service_6pcnbro", "template_nktigru", tempParams)
+			.then(function (response) {
+				console.log("sucess", response.status);
+				location.reload();
+			});
+	});
+	error_message.style.padding = "10px";
 
-	//Verify the TextBox value.
-	if (msg.value.trim() != "") {
-		//Enable the TextBox when TextBox has value.
-		btnSubmit.disabled = false;
-	} else {
-		//Disable the TextBox when TextBox is empty.
-		btnSubmit.disabled = true;
+	var text;
+	if (name.length <= 0) {
+		text = "Please Enter Your Name";
+		error_message.innerHTML = text;
+		return false;
 	}
+	if (email.indexOf("@") == -1 || email.length < 6) {
+		text = "Please Enter a Valid Email Address";
+		error_message.innerHTML = text;
+		return false;
+	}
+	if (message.length <= 0) {
+		text = "Please Enter a Message";
+		error_message.innerHTML = text;
+		return false;
+	}
+	alert("Form Submitted Successfully!");
+	return true;
 }
-let sendButton = document.getElementById("send");
-
-sendButton.addEventListener("click", function (event) {
-	event.preventDefault();
-	var tempParams = {
-		from_name: document.getElementById("fromName").value,
-		from_email: document.getElementById("fromEmail").value,
-		message: document.getElementById("msg").value,
-	};
-	emailjs
-		.send("service_lzkzpo6", "template_7h9n81z", tempParams)
-		.then(function (response) {
-			console.log("sucess", response.status);
-			location.reload();
-			alert(
-				"THANK YOU FOR YOUR MESSAGE, I WILL GET BACK TO YOU AS SOON AS POSSIBLE"
-			);
-		});
-});
